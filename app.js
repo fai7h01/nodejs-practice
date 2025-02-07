7377cfb1954bbc35037c7c48f7f8666a68ea1e03
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-07 17:13:02 +0400

    express

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 
+app.get('/:username/messages', (req, res) => {
+    console.log(req.params);
+    res.end();
+})
+
+app.get('/:username/messages/:messageId', (req, res) => {
+    let username = req.params.username;
+    let messageId = req.params.messageId;
+    console.log('MessageId: ' + messageId + ". User: " + username);
+    res.end();
+})
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(PORT, () => {
