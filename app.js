1a72c071af1ab043cd75158e874a7446c8a1589c
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-07 17:01:56 +0400

    express

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => {
+    console.log(`Basic Express App - listening port: ${PORT}`);
+}); 
